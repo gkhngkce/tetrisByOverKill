b8a23b3f2fc8bb3e1706cdccd8f0182c2a229a03
gkhngkce <gokhangokce8207@gmail.com>
2021-11-01 22:37:56 +0300

    Some syntax & comment line improvement

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@
 using namespace sf;
 using namespace std;
 
+//Game field variables
 const int fieldRow = 20;
 const int fieldColumn = 10;
 
+//Defining the game field
 int gameField[fieldRow][fieldColumn] = { 0 };
 
+// ??? point of tetrominos ???
 struct Point
 {
 	int x, y;
 } a[4], b[4];
 
+//Defining tetrominos
 int tetrominos[7][4] =
 {
 	1,3,5,7, // I
@@ -25,14 +29,21 @@ int tetrominos[7][4] =
 	2,3,4,5, // O
 };
 
-
+ //Collusion detection function
 bool collisionDetection()
 {
 	for (int i = 0; i < 4; i++)
-		if (a[i].x < 0 || a[i].x >= fieldColumn || a[i].y >= fieldRow) return 0;
-		else if (gameField[a[i].y][a[i].x]) return 0;
-
-		return 1;
+	{
+		if (a[i].x < 0 || a[i].x >= fieldColumn || a[i].y >= fieldRow)
+		{
+			return 0;
+		}
+		else if (gameField[a[i].y][a[i].x])
+		{
+			return 0;
+		}
+	}
+	return 1;
 };
 
 int theGame()
@@ -43,13 +54,11 @@ int theGame()
 	tiles.loadFromFile("sources/tiles.png");
 	bground.loadFromFile("sources/background.png");
 	outFrame.loadFromFile("sources/frame.png");
-
 	Sprite sprite(tiles), background(bground), frame(outFrame);
+	Clock clock;
 
 	int dx = 0; bool isRotated = 0; int colorNum = 1;
 	float timer = 0, delay = 0.3;
-
-	Clock clock;
 
 	while (window.isOpen())
 	{
@@ -61,23 +70,46 @@ int theGame()
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
+			{
 				window.close();
-
+			}
 			if (event.type == Event::KeyPressed)
 			{
-				if (event.key.code == Keyboard::Up) isRotated = true;
-				if (event.key.code == Keyboard::Left) dx = -1;
-				if (event.key.code == Keyboard::Right) dx = 1;
+				if (event.key.code == Keyboard::Up)
+				{
+					isRotated = true;
+				}
+				if (event.key.code == Keyboard::Left)
+				{
+					dx = -1;
+				}
+				if (event.key.code == Keyboard::Right)
+				{
+					dx = 1;
+				}
 			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			delay = 0.05;
+		}
 
-		//// <- Move -> ///
-		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
-		if (!collisionDetection()) for (int i = 0; i < 4; i++) a[i] = b[i];
+		// <- Move ->
+		for (int i = 0; i < 4; i++) 
+		{ 
+			b[i] = a[i]; 
+			a[i].x += dx; 
+		}
+		if (!collisionDetection())
+		{
+			for (int i = 0; i < 4; i++) 
+			{ 
+				a[i] = b[i]; 
+			}
+		}
 
-		//////isRotated//////
+		//Check whether current tetromino is rotated or not ?
 		if (isRotated)
 		{
 			Point centerOfRotation = a[1]; //center of rotation
@@ -88,60 +120,80 @@ int theGame()
 				a[i].x = centerOfRotation.x - x;
 				a[i].y = centerOfRotation.y + y;
 			}
-			if (!collisionDetection()) for (int i = 0; i < 4; i++) a[i] = b[i];
-		}
-
-		///////Tick//////
-		if (timer > delay)
-		{
-			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
-
 			if (!collisionDetection())
 			{
-				for (int i = 0; i < 4; i++) gameField[b[i].y][b[i].x] = colorNum;
+				for (int i = 0; i < 4; i++)
+				{
+					a[i] = b[i];
+				}
+			}
+		}
 
-				// Feature: assign a fixed color to each shape
+		//Vertical motion
+		if (timer > delay)
+		{
+			for (int i = 0; i < 4; i++) 
+			{ 
+				b[i] = a[i]; 
+				a[i].y += 1; 
+			}
+			if (!collisionDetection())
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					gameField[b[i].y][b[i].x] = colorNum;
+				}
+				//random tetromino generation and coloring
 				int n = rand() % 7;
 				colorNum = n + 1;
 				for (int i = 0; i < 4; i++)
 				{
-					a[i].x = tetrominos[n][i] % 2;
-					a[i].y = tetrominos[n][i] / 2;
+					a[i].x = tetrominos[n][i] % 2; //getting x coordinate of tetromino
+					a[i].y = tetrominos[n][i] / 2; //getting y coordinate of tetromino
 				}
 			}
-
 			timer = 0;
 		}
 
-		///////collisionDetection lines//////////
+		//Line clear check
 		int k = fieldRow - 1;
 		for (int i = fieldRow - 1; i > 0; i--)
 		{
 			int count = 0;
 			for (int j = 0; j < fieldColumn; j++)
 			{
-				if (gameField[i][j]) count++;
+				if (gameField[i][j])
+				{
+					count++;
+				}
 				gameField[k][j] = gameField[i][j];
 			}
-			if (count < fieldColumn) k--;
+			if (count < fieldColumn)
+			{
+				k--;
+			}
 		}
 
 		dx = 0; isRotated = 0; delay = 0.3;
 
-		/////////draw//////////
+		//Drawing to the screen
 		window.clear(Color::White);
 		window.draw(background);
 
 		for (int i = 0; i < fieldRow; i++)
+		{
 			for (int j = 0; j < fieldColumn; j++)
 			{
-				if (gameField[i][j] == 0) continue;
+				if (gameField[i][j] == 0)
+				{
+					continue;
+				}
 				sprite.setTextureRect(IntRect(gameField[i][j] * 18, 0, 18, 18));
 				sprite.setPosition(j * 18, i * 18);
 				sprite.move(28, 31); //offset
 				window.draw(sprite);
 			}
-
+		}
 		for (int i = 0; i < 4; i++)
 		{
 			sprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
@@ -149,7 +201,6 @@ int theGame()
 			sprite.move(28, 31); //offset
 			window.draw(sprite);
 		}
-
 		window.draw(frame);
 		window.display();
 	}
