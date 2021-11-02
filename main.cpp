0afb8608ff1fe7f18ad9914201e7c60dc6309ad3
denizakbaba <35350131+denizakbaba@users.noreply.github.com>
2021-11-03 00:32:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,13 +23,13 @@ struct Point
 //Defining tetrominos
 int tetrominos[7][4] =
 {
-	1,3,5,7, // I
+	3,5,4,7, // T
 	2,4,5,7, // Z
 	3,5,4,6, // S
-	3,5,4,7, // T
+	2,3,4,5, // O
+	1,3,5,7, // I
 	2,3,5,7, // L
 	3,5,7,6, // J
-	2,3,4,5, // O
 };
 
 Text gameOverText;
